3e5b598878f070942c9d55bd6518acd09b026c31
S <taz3008@msn.com>
2021-07-31 15:01:40 +0200

    Moved my own variables into new class

---
+++ LostRuinsMod/CustomGameInfo.cs
@@ -0,0 +1,16 @@
+﻿using Nunppong;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace LostRuinsMod
+{
+    static class CustomGameInfo
+    {
+        public static BindingFlags flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
+        public static RectTransform ngPlusRect;
+        public static LabelView newGamePlusButton;
+        public static List<DropItem> gameItems = new List<DropItem>();
+        public static bool IsNGP { get; set; } = false;
+    }
+}
