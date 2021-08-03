2e99d48f7ae3d246b745184e0865895fb200202f
S <taz3008@msn.com>
2021-08-03 17:16:25 +0200

    IsNGP getter/setter

---
+++ LostRuinsMod/StageManagerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Nunppong;
+
+namespace LostRuinsMod
+{
+    [HarmonyPatch]
+    class StageManagerPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StageManager), "StartNewGame")]
+        public static void StageManagerStartNewGamePrePatch()
+        {
+            CustomGameInfo.IsNGP = true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StageManager), "LoadTitle")]
+        public static void SaveLoadManagerClearPrePatch()
+        {
+            CustomGameInfo.IsNGP = false;
+        }
+    }
+}
