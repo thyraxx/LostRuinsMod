d787cd2c2fd1390f520e28c78009164427f4a3fb
S <taz3008@msn.com>
2021-08-05 00:26:23 +0200

    Added complete item list

---
+++ LostRuinsMod/GameManagerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Nunppong;
+using Nunppong.Datasheet;
+using System;
+using System.Linq;
+
+namespace LostRuinsMod
+{
+    [HarmonyPatch]
+    class GameManagerPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameManager), "LoadResourceAssets")]
+        public static void GameManagerLoadResourceAssetsPostPatch(GameManager __instance)
+        {
+
+            foreach (DropItem dropItem in __instance.DropItemManager.DropItems)
+            {
+                CustomGameInfo.gameItems.Add(dropItem);
+            }
+        }
+    }
+}
