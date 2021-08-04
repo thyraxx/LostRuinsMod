04caf9eabde881af02b056196ff1d520ae39edf8
S <taz3008@msn.com>
2021-08-05 00:38:57 +0200

    Commented
    Might look somewhere in the future into it

--- LostRuinsMod/MovementPatch.cs
+++ LostRuinsMod/MovementPatch.cs
@@ -8,35 +8,35 @@ namespace LostRuinsMod
 	class MovementPatch
 	{
 
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(PlayerHandleWeapon), "UpdateWeaponState")]
-        public static bool UpdateWeaponState_PrePatch(PlayerHandleWeapon __instance)
-        {
-            //System.Console.WriteLine("INIT");
-            if (__instance.State.Current == PlayerHandleWeapon.States.Attack)
-            {
-                //__instance.State.Current = PlayerHandleWeapon.States.Idle;
-                //__instance.Player.ForceController.SetHorizontalForce(__instance.Player.ForceController.CurrentForce.x);
-                __instance.Player.ForceController.SetHorizontalForce(__instance.Player.ForceController.CurrentForce.x);
-                if (__instance.State.StateChangedTime + __instance.CurrentWeaponSetting.WeaponDuration < Time.time)
-                {
-                    __instance.StopWeapon();
-                    __instance.State.Current = PlayerHandleWeapon.States.Delay;
-                    __instance.State.Current = PlayerHandleWeapon.States.DelayToIdle;
-                    //return false;
+        //[HarmonyPrefix]
+        //[HarmonyPatch(typeof(PlayerHandleWeapon), "UpdateWeaponState")]
+        //public static bool UpdateWeaponState_PrePatch(PlayerHandleWeapon __instance)
+        //{
+        //    //System.Console.WriteLine("INIT");
+        //    if (__instance.State.Current == PlayerHandleWeapon.States.Attack)
+        //    {
+        //        //__instance.State.Current = PlayerHandleWeapon.States.Idle;
+        //        //__instance.Player.ForceController.SetHorizontalForce(__instance.Player.ForceController.CurrentForce.x);
+        //        __instance.Player.ForceController.SetHorizontalForce(__instance.Player.ForceController.CurrentForce.x);
+        //        if (__instance.State.StateChangedTime + __instance.CurrentWeaponSetting.WeaponDuration < Time.time)
+        //        {
+        //            __instance.StopWeapon();
+        //            __instance.State.Current = PlayerHandleWeapon.States.Delay;
+        //            __instance.State.Current = PlayerHandleWeapon.States.DelayToIdle;
+        //            //return false;
 
 
-                }
-            }
-            //else if (__instance.State.Current == PlayerHandleWeapon.States.Delay)
-            //{
-            //    __instance.State.Current = PlayerHandleWeapon.States.Idle;
+        //        }
+        //    }
+        //    //else if (__instance.State.Current == PlayerHandleWeapon.States.Delay)
+        //    //{
+        //    //    __instance.State.Current = PlayerHandleWeapon.States.Idle;
 
-            //    __instance.Player.ForceController.SetHorizontalForce(__instance.Player.ForceController.CurrentForce.x);
-            //    return false;
-            //}
-            return true;
-        }
+        //    //    __instance.Player.ForceController.SetHorizontalForce(__instance.Player.ForceController.CurrentForce.x);
+        //    //    return false;
+        //    //}
+        //    return true;
+        //}
 
         //      [HarmonyPrefix]
         //[HarmonyPatch(typeof(PlayerHandleWeapon), "TryAttack")]
@@ -55,11 +55,11 @@ namespace LostRuinsMod
         //	return true;
         //}
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PlayerHandleWeapon), "CanMove")]
-        public static void CanMove(bool __result)
-        {
-            __result = false;
-        }
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(PlayerHandleWeapon), "CanMove")]
+        //public static void CanMove(bool __result)
+        //{
+        //    __result = false;
+        //}
     }
 }
